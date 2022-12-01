f5f991965399228ca4c60df4077318890dc6e71c
parsa <parsa@mail.com>
2022-12-01 20:49:30 +0330

    create models and migrate and menu.html and contact.html

--- A/blog/models.py
+++ A/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+class Category(models.Model):
+	title = models.CharField(max_length=128, null=False, blank=False)
+
+	def __str__(self):
+		return self.name
+
+class Product(models.Model):
+	category = models.ManyToManyField(Category, related_name='products')
+	name = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200, unique=True)
+	image = models.ImageField(null=False, blank=True)
+	description = RichTextField()
+	s_price = models.IntegerField()
+	t_price = models.IntegerField()
+	available = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.name
