b8272587bd26be66115ba6c6a3d72d1930e220ad
Parsa <parsa.khoshvaghti@gmail.com>
2023-02-08 07:05:51 +0330

    Static Changes

--- thecafe/settings.py
+++ thecafe/settings.py
@@ -79,6 +79,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
--- thecafe/urls.py
+++ thecafe/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.i18n import JavaScriptCatalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
+    path('js-catalog', JavaScriptCatalog.as_view() , name='js-catalog'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
